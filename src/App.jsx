44069b4a752ffa94d902961dfb2c100b23684c36
Ayoubhj866 <ayoubelhajji866@gmail.com>
2025-12-13 22:14:06 +0100

    start process workflow section design

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from '@/components/hero/hero';
 import About from '@/components/about/about';
 import Services from '@/components/services/services';
 import Stats from '@/components/stats-section/stats';
+import Workflow from '@/components/process-workflow/workflow';
 
 const App = () => {
     return <div className="min-h-screen relative">
@@ -11,6 +12,7 @@ const App = () => {
         <About />
         <Services />
         <Stats />
+        <Workflow />
         <div className='h-[200px]'>
 
         </div>
---
+++ src/components/process-workflow/workflow.jsx
@@ -0,0 +1,13 @@
+import LayoutSection from "@/components/layouts/layout-section";
+import SectionHeader from "@/components/section-header";
+
+const Workflow = () => {
+    return (
+        <LayoutSection>
+            <SectionHeader title="Tattoo Workflow" subtitle="Our Process" />
+        </LayoutSection>
+    );
+};
+
+
+export default Workflow;
